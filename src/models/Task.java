fa7169bdd695354d15e8f076ed01cffca6efe251
yoshiki.tanizawa <beautifulsunset777@gmail.com>
2024-03-07 23:52:54 +0900

    ShowServlet.javaを作成

--- src/models/Task.java
+++ src/models/Task.java
@@ -15,20 +15,17 @@ import javax.persistence.Table;
 
 @NamedQueries({
     @NamedQuery(
-        name = "getAllMessages",
-        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+        name = "getAllTasks",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
     )
 })
 
 @Table(name = "tasks")
-public class Message {
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
@@ -47,13 +44,6 @@ public class Message {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
     public String getContent() {
         return content;
